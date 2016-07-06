8de6d6591561e417c7b6e7c15e02bfe143e35515
logitha <loga848.456@gmail.com>
2016-07-06 11:30:17 +0530

    Create c1.c

---
+++ c1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int c=0,n,i,j,n1,a;
+char s[30][30];
+printf("\nenter the number of array");
+scanf("%d",&j);
+for(i=0;i<n1;i++);
+{
+scanf("%s",&s[i]);
+}
+printf("to find number of words inthe length");
+scanf("%d",&n);
+for(i=0;i<n1;i++)
+{
+a=strlen(s[i]);
+if(a==n)
+c++;
+}
+printf("%d",c);
+return 0;
+}
